3c7df2ee10d74d04406036201a9eab1a7c3ddd5e
chad-iris <chad.trabant@iris.edu>
2020-08-07 16:32:48 -0700

    Rename custom.js to sidebar_context for clarity, update styling

--- docs/conf.py
+++ docs/conf.py
@@ -29,11 +29,6 @@ version = '1.1.0'
 # Documentation version, schema + date
 doc_version = version + ' (2020-08-01)'
 
-def setup(app):
-    app.add_css_file('css/custom.css')
-    app.add_css_file('css/theme_overrides.css')
-    app.add_js_file('js/custom.js')
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -112,3 +107,13 @@ navigation_depth = -2
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+  'css/custom.css',
+  'css/theme_overrides.css',
+]
+
+html_js_files = [
+  'js/sidebar_context.js'
+]
+
